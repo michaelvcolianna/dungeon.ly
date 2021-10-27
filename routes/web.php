3742c526281860efa03f576508d66b25fd161120
Michael Colianna <mcolianna@cronin-co.com>
2021-10-26 20:17:19 -0400

    Tweaks the routes file

--- routes/web.php
+++ routes/web.php
@@ -13,10 +13,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', function() {
+    return (auth()->user())
+        ? redirect()->route('dashboard')
+        : view('welcome')
+        ;
 });
 
-Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
+Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function() {
     return view('dashboard');
 })->name('dashboard');
