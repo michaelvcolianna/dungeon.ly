0d04f6c75ceb0916731e4d676999217c32f8cbba
Michael Colianna <mcolianna@cronin-co.com>
2021-10-28 21:00:49 -0400

    Makes the form fields better

--- resources/views/components/fields/text.blade.php
+++ resources/views/components/fields/text.blade.php
@@ -10,6 +10,6 @@
 
     <input
         type="{{ $type }}" id="{{ $id }}" wire:model.debounce.500ms="{{ $id }}"
-        {!! $attributes->merge(['class' => 'border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 rounded-md shadow-sm']) !!}
+        {!! $attributes->merge(['class' => 'border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 rounded-md shadow-sm w-full']) !!}
     />
 </div>
--- resources/views/components/fields/textarea.blade.php
+++ resources/views/components/fields/textarea.blade.php
@@ -10,6 +10,6 @@
 
     <textarea
         id="{{ $id }}" wire:model.debounce.750ms="{{ $id }}" rows="{{ $rows }}"
-        {!! $attributes->merge(['class' => 'border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 rounded-md shadow-sm']) !!}
+        {!! $attributes->merge(['class' => 'border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 rounded-md shadow-sm w-full resize-none']) !!}
     ></textarea>
 </div>
