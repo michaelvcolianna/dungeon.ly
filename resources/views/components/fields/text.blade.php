ab5a7634aefeb63268ce371c864ec384aa9b4e3e
Michael Colianna <mcolianna@cronin-co.com>
2021-10-28 20:58:33 -0400

    Unchains the form fields from Jetstream

--- resources/views/components/fields/text.blade.php
+++ resources/views/components/fields/text.blade.php
@@ -1,6 +1,15 @@
 @props(['id', 'label', 'type' => 'text'])
 
 <div>
-    <x-jet-label for="{{ $id }}">{!! $label !!}</x-jet-label>
-    <x-jet-input type="{{ $type }}" id="{{ $id }}" wire:model.debounce.500ms="{{ $id }}" />
+    <label
+        for="{{ $id }}"
+        {{ $attributes->merge(['class' => 'block font-medium text-sm text-gray-700']) }}
+    >
+        {{ $label }}
+    </label>
+
+    <input
+        type="{{ $type }}" id="{{ $id }}" wire:model.debounce.500ms="{{ $id }}"
+        {!! $attributes->merge(['class' => 'border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 rounded-md shadow-sm']) !!}
+    />
 </div>
--- resources/views/components/fields/textarea.blade.php
+++ resources/views/components/fields/textarea.blade.php
@@ -1,6 +1,15 @@
 @props(['id', 'label', 'rows' => 5])
 
 <div>
-    <x-jet-label for="{{ $id }}">{!! $label !!}</x-jet-label>
-    <textarea id="{{ $id }}" wire:model.debounce.750ms="{{ $id }}" rows="{{ $rows }}" {!! $attributes->merge(['class' => 'border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 rounded-md shadow-sm']) !!}></textarea>
+    <label
+        for="{{ $id }}"
+        {{ $attributes->merge(['class' => 'block font-medium text-sm text-gray-700']) }}
+    >
+        {{ $label }}
+    </label>
+
+    <textarea
+        id="{{ $id }}" wire:model.debounce.750ms="{{ $id }}" rows="{{ $rows }}"
+        {!! $attributes->merge(['class' => 'border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 rounded-md shadow-sm']) !!}
+    ></textarea>
 </div>
