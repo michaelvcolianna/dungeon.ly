769d0db5b9ae93c8a56627097abbed9cce2c50d0
Michael Colianna <mcolianna@cronin-co.com>
2021-11-12 16:05:04 -0500

    Tweaks the character migration and a view

--- database/migrations/2021_10_26_011116_create_characters_table.php
+++ database/migrations/2021_10_26_011116_create_characters_table.php
@@ -21,81 +21,20 @@ class CreateCharactersTable extends Migration
             $table->timestamps();
             $table->softDeletes();
 
-            // Character information
-            $table->string('name')->nullable();
-            $table->string('class_level')->nullable();
-            $table->string('background')->nullable();
-            $table->string('race')->nullable();
-            $table->string('alignment')->nullable();
-            $table->string('experience_points')->nullable();
-            $table->string('age')->nullable();
-            $table->string('height')->nullable();
-            $table->string('weight')->nullable();
-            $table->string('eyes')->nullable();
-            $table->string('skin')->nullable();
-            $table->string('hair')->nullable();
-            $table->text('personality_traits')->nullable();
-            $table->text('ideals')->nullable();
-            $table->text('bonds')->nullable();
-            $table->text('flaws')->nullable();
-            $table->text('other_proficiencies_languages')->nullable();
-            $table->text('features_traits')->nullable();
-            $table->string('appearance', 2048)->nullable();
-            $table->text('allies_organizations')->nullable();
-            $table->string('affiliation')->nullable();
-            $table->string('symbol', 2048)->nullable();
-            $table->text('backstory')->nullable();
-            $table->text('additional_features_traits')->nullable();
-
-            // Attributes
-            $table->json('strength')->nullable();
-            $table->json('dexterity')->nullable();
-            $table->json('constitution')->nullable();
-            $table->json('intelligence')->nullable();
-            $table->json('wisdom')->nullable();
-            $table->json('charisma')->nullable();
-            $table->string('passive_wisdom')->nullable();
-            $table->string('inspiration')->nullable();
-            $table->string('proficiency_bonus')->nullable();
-            $table->string('armor_class')->nullable();
-            $table->string('initiative')->nullable();
-            $table->string('speed')->nullable();
-            $table->json('hit_points')->nullable();
-            $table->json('hit_dice')->nullable();
-            $table->json('death_saves')->nullable();
-            $table->string('spellcasting_class')->nullable();
-            $table->string('spellcasting_ability')->nullable();
-            $table->string('spell_save_dc')->nullable();
-            $table->string('spell_attack_bonus')->nullable();
-            $table->json('spell_levels')->nullable();
-            $table->json('spells')->nullable();
-
-            // Skills
-            $table->json('acrobatics')->nullable();
-            $table->json('animal_handling')->nullable();
-            $table->json('arcana')->nullable();
-            $table->json('athletics')->nullable();
-            $table->json('deception')->nullable();
-            $table->json('history')->nullable();
-            $table->json('insight')->nullable();
-            $table->json('intimidation')->nullable();
-            $table->json('investigation')->nullable();
-            $table->json('medicine')->nullable();
-            $table->json('nature')->nullable();
-            $table->json('perception')->nullable();
-            $table->json('performance')->nullable();
-            $table->json('persuasion')->nullable();
-            $table->json('religion')->nullable();
-            $table->json('sleight_of_hand')->nullable();
-            $table->json('stealth')->nullable();
-            $table->json('survival')->nullable();
-
-            // Gear
-            $table->json('weapons')->nullable();
-            $table->text('attacks_spellcasting')->nullable();
-            $table->json('money')->nullable();
-            $table->text('equipment')->nullable();
-            $table->text('treasure')->nullable();
+            // Dynamic configurations
+            foreach(config('dnd.character') as $field => $config)
+            {
+                if(isset($config['size']))
+                {
+                    // Image fields
+                    $table->{$config['column']}($field, $config['size'])->nullable();
+                }
+                else
+                {
+                    // All other fields
+                    $table->{$config['column']}($field)->nullable();
+                }
+            }
         });
     }
 
